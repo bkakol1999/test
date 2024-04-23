a2739bb9c502cd9e5af9f2c9db20e6d8815426e3
Bart Kakol <bart.kakol@shepherdglobal.com>
2024-04-23 16:12:48 +0100

    implemented experimental add function

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
 
-echo 123;
-
-
 function add(int $i, int $j) : int {
-    return $i + $j;
+    return ($i + $j);
 }
+
+function addExperimental(int $i, int $j) : int {
+    while($j != 0) {
+        $i++;
+        $j--;
+    }
+    return $i;
+}
+
+print_r(add(30,50));
+print_r("\n");
+print_r(addExperimental(30,50));
